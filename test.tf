bf23123ab9856c0d59d817433a790f1c5c3dd225
tlsaikishore <tlsaikishore@gmail.com>
2021-11-23 14:34:23 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mybucket" {
 
   server_side_encryption_configuration {
     rule {
-      apply_server_side_encryption_by_default {
+      apply_server_side_encryption_by_default { 
       }
     }
   }
